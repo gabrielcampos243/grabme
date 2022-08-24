3c1f56aa8aba6bb25059beb8a11c3a3fd0ffad38
Martim Correia <correia_martim@hotmail.com>
2022-08-24 10:47:35 +0100

    addig user id and a use can only make a serice at the time

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,4 @@
 class Partner < ApplicationRecord
+  # validates :user_id, uniqueness: {  message: "You've already made a comment!" }
   has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :partners, only: [:create, :index, :show, :destroy, :new, :edit,:update] do
+    collection do
+      get :my_service
+    end
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :show, :destroy]
