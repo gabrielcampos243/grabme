31e5e5ff20f45c8d4cf0ad169b7f3d012915bf31
Martim Correia <correia_martim@hotmail.com>
2022-08-24 16:20:01 +0100

    Some upgrades

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -31,7 +31,7 @@ class BookingsController < ApplicationController
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to partner_path(@partner)
+    redirect_to bookings_path
   end
 
   private
